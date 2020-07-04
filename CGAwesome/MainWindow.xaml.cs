513daf685470f6494acdac63659c4838f370e41a
CGAwesome <jochance+CGAwesome@gmail.com>
2020-07-04 02:59:22 -0500

    More refactor/teardown of xaml code-behind

--- CGAwesome/MainWindow.xaml.cs
+++ CGAwesome/MainWindow.xaml.cs
@@ -66,12 +66,12 @@ namespace CGAwesome
             var packRoot = new DirectoryInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\ExportedPacks\\{packName}");
             var functionName = optionFunctionName.Text;
 
-            Cleanup(packRoot, packName, functionName);
+            MinecraftPackageManagement.Cleanup(packRoot, packName, functionName);
 
             var fillBlockDictionary = MinecraftColorConvert.GetColorToMinecraftBlockDictionary(optionFillBlock.Text, (bool)optionTransparencyMask.IsChecked, (bool)optionJava.IsChecked, optionTransparencyBlock.Text);
             var fillCommands = ImageProcessing.GetFillCommandsFromImage(ProcessedImages[MainImage], fillBlockDictionary, GetOrientationFromOptionGroup());
 
-            ExportPackage(fillCommands, packName, functionName, optionNewVersion.Text, packRoot);
+            MinecraftPackageManagement.ExportPackage(fillCommands, packName, functionName, optionNewVersion.Text, packRoot);
 
             ZipFile.CreateFromDirectory($"{packRoot.FullName}", $"{packRoot.Parent.FullName}\\{packName}.mcpack");
 
@@ -80,85 +80,6 @@ namespace CGAwesome
             if (Directory.Exists(path))
             {
                 Process.Start(path);
-            }
-        }
-
-        private void Cleanup(DirectoryInfo packRoot, string packageName, string functionName)
-        {
-            if (!Directory.Exists(packRoot.FullName)) Directory.CreateDirectory(packRoot.FullName);
-
-            if (!Directory.Exists($"{packRoot.FullName}\\functions")) Directory.CreateDirectory($"{packRoot.FullName}\\functions");
-            if (File.Exists($"{packRoot.FullName}\\functions\\{functionName}.mcfunction")) File.Delete($"{packRoot.FullName}\\functions\\{functionName}.mcfunction");
-            if (File.Exists($"{packRoot.Parent.FullName}\\{packageName}.mcpack"))
-            {
-                File.Delete($"{packRoot.Parent.FullName}\\{packageName}.mcpack");
-            }
-        }
-
-        private static void ExportPackage(StringBuilder fillCommands, string packageName, string functionName, string newVersion, DirectoryInfo packRoot)
-        {
-            using (StreamWriter sw = new StreamWriter($"{packRoot.FullName}\\functions\\{functionName}.mcfunction"))
-            {
-                sw.Write(fillCommands.ToString());
-            }
-            //TODO: Open behavior_pack_manifest and update version? Implement save/import of other scripts? Function slots? /function cga1, cga2, etc?
-
-            if (!File.Exists($"{packRoot.FullName}\\manifest.json"))
-            {
-                using (StreamReader sr = new StreamReader(new FileStream($"{AppDomain.CurrentDomain.BaseDirectory}\\Manifest Templates\\behavior_pack_manifest.json", FileMode.Open)))
-                using (StreamWriter sw = new StreamWriter($"{packRoot.FullName}\\manifest.json"))
-                {
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-
-                        switch (line.Split(':')[0].Trim().Replace("\"", ""))
-                        {
-                            case "uuid":
-                                sw.WriteLine($"\"uuid\":\"{Guid.NewGuid().ToString()}\",");
-                                break;
-                            case "description":
-                                sw.WriteLine($"\"description\":\"{packageName}\",");
-                                break;
-                            case "name":
-                                sw.WriteLine($"\"name\":\"{packageName}\",");
-                                break;
-                            default:
-                                sw.WriteLine(line);
-                                break;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                var templateManifestPath = $"{packRoot.FullName}\\manifest.json";
-                var newManifestPath = $"{packRoot.FullName}\\newmanifest.json";
-                var oldManifestPath = $"{packRoot.FullName}\\oldmanifest.json";
-
-                if (File.Exists(oldManifestPath)) File.Delete(oldManifestPath);
-
-                using (StreamReader sr = new StreamReader(new FileStream(templateManifestPath, FileMode.Open)))
-                using (StreamWriter sw = new StreamWriter(newManifestPath))
-                {
-                    while (!sr.EndOfStream)
-                    {
-                        string line = sr.ReadLine();
-                        string version = newVersion;
-                        switch (line.Split(':')[0].Trim().Replace("\"", ""))
-                        {
-                            case "version":
-                                sw.WriteLine($"\"version\": [{version}],");
-                                break;
-                            default:
-                                sw.WriteLine(line);
-                                break;
-                        }
-                    }
-                }
-
-                File.Replace(newManifestPath, templateManifestPath, oldManifestPath);
-                File.Delete(oldManifestPath);
             }
         }
 
---
+++ CGAwesome/MinecraftPackageManagement.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CGAwesome
+{
+    public static class MinecraftPackageManagement
+    {
+        public static void Cleanup(DirectoryInfo packRoot, string packageName, string functionName)
+        {
+            if (!Directory.Exists(packRoot.FullName)) Directory.CreateDirectory(packRoot.FullName);
+
+            if (!Directory.Exists($"{packRoot.FullName}\\functions")) Directory.CreateDirectory($"{packRoot.FullName}\\functions");
+            if (File.Exists($"{packRoot.FullName}\\functions\\{functionName}.mcfunction")) File.Delete($"{packRoot.FullName}\\functions\\{functionName}.mcfunction");
+            if (File.Exists($"{packRoot.Parent.FullName}\\{packageName}.mcpack"))
+            {
+                File.Delete($"{packRoot.Parent.FullName}\\{packageName}.mcpack");
+            }
+        }
+
+        public static void ExportPackage(StringBuilder fillCommands, string packageName, string functionName, string newVersion, DirectoryInfo packRoot)
+        {
+            using (StreamWriter sw = new StreamWriter($"{packRoot.FullName}\\functions\\{functionName}.mcfunction"))
+            {
+                sw.Write(fillCommands.ToString());
+            }
+            //TODO: Open behavior_pack_manifest and update version? Implement save/import of other scripts? Function slots? /function cga1, cga2, etc?
+
+            if (!File.Exists($"{packRoot.FullName}\\manifest.json"))
+            {
+                using (StreamReader sr = new StreamReader(new FileStream($"{AppDomain.CurrentDomain.BaseDirectory}\\Manifest Templates\\behavior_pack_manifest.json", FileMode.Open)))
+                using (StreamWriter sw = new StreamWriter($"{packRoot.FullName}\\manifest.json"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+
+                        switch (line.Split(':')[0].Trim().Replace("\"", ""))
+                        {
+                            case "uuid":
+                                sw.WriteLine($"\"uuid\":\"{Guid.NewGuid().ToString()}\",");
+                                break;
+                            case "description":
+                                sw.WriteLine($"\"description\":\"{packageName}\",");
+                                break;
+                            case "name":
+                                sw.WriteLine($"\"name\":\"{packageName}\",");
+                                break;
+                            default:
+                                sw.WriteLine(line);
+                                break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var templateManifestPath = $"{packRoot.FullName}\\manifest.json";
+                var newManifestPath = $"{packRoot.FullName}\\newmanifest.json";
+                var oldManifestPath = $"{packRoot.FullName}\\oldmanifest.json";
+
+                if (File.Exists(oldManifestPath)) File.Delete(oldManifestPath);
+
+                using (StreamReader sr = new StreamReader(new FileStream(templateManifestPath, FileMode.Open)))
+                using (StreamWriter sw = new StreamWriter(newManifestPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string version = newVersion;
+                        switch (line.Split(':')[0].Trim().Replace("\"", ""))
+                        {
+                            case "version":
+                                sw.WriteLine($"\"version\": [{version}],");
+                                break;
+                            default:
+                                sw.WriteLine(line);
+                                break;
+                        }
+                    }
+                }
+
+                File.Replace(newManifestPath, templateManifestPath, oldManifestPath);
+                File.Delete(oldManifestPath);
+            }
+        }
+    }
+}
